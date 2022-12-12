f97910c3fdd9a45e73e47cf2abbbf290ce861ef8
Jorge Mendez <jorge.mendezgines@gmail.com>
2022-12-13 00:34:04 +0100

    almost done with albums

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express"); // require the express.js module
+const app = express(); // create an instance of the express app
+app.use(express.static('public'));
+
+// define routes for the different pages of your website
+app.get("/", (req, res) => {
+  res.status(200).sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).sendFile(__dirname + "/views/about.html");
+});
+
+app.get("/photos", (req, res) => {
+  res.status(200).sendFile(__dirname + "/views/photos-kanye.html");
+});
+
+app.get("/works", (req, res) => {
+  res.status(200).sendFile(__dirname + "/views/works-kanye.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
